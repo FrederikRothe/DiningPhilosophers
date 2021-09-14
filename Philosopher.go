9312599eb102e05aa4628d4df11d7c42e7badf64
Frederik Rothe <66801011+FrederikRothe@users.noreply.github.com>
2021-09-14 09:26:00 +0200

    Philosopher (#2)
    * Starter classes
    
    * First implementation of philosopher.
    
    * Further improvements.
    
    Co-authored-by: CasparMarschall <cafm@itu.dk>

--- Philosopher.go
+++ Philosopher.go
@@ -1,9 +1,37 @@
 package main
 
-import (
-	"fmt"
-)
+type philosopher struct{
+	eating bool
+	timesEaten int
+	leftFork *fork
+	rightFork *fork
+}
 
-func Philosopher() {
-	fmt.Println("Need code")
+func NewPhilosopher(leftFork *fork, rightFork *fork) *philosopher {
+	p := philosopher{leftFork: leftFork, rightFork: rightFork}
+	p.eating = false
+	p.timesEaten = 0
+	return &p
+}
+
+func Start(p philosopher) {
+	for {
+		var rf, lf bool
+		select {
+		case rf <- rightFork.output:
+			rightFork.waiter.Lock()
+			select {
+			case lf <- leftFork.output:
+				leftFork.waiter.Lock()
+				p.eating = true
+				p.timesEaten++
+				fmt.Println("Eating nam nam")
+				time.Sleep(300 * time.Millisecond)
+				leftFork.input <- 1
+				rightFork.input <- 1
+				leftFork.waiter.Unlock()
+			}
+			rightFork.waiter.Unlock()
+		}	
+	}
 }
