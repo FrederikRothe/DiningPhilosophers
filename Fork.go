1c9c5443069a046b67c1c4334d056842236a5b80
CasparMarschall <cafm@itu.dk>
2021-09-14 09:22:49 +0200

    Started fork class

--- Fork.go
+++ Fork.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"sync"
 )
 
-func Fork() {
-	fmt.Println("Need code")
+type fork struct {
+	waiter    sync.Mutex
+	timesUsed int
+}
+
+func Fork(f fork) {
+	output := make(chan bool, 1)
+	input := make(chan int, 1)
+
+	for {
+		f.timesUsed += <-input
+		output <- false
+	}
 }
