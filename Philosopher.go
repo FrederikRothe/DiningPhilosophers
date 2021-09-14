be04350fffb341ccd69ec3889f281d5fefb8e87f
Frakty <66801011+FrederikRothe@users.noreply.github.com>
2021-09-14 09:22:12 +0200

    Further improvements.

--- Philosopher.go
+++ Philosopher.go
@@ -2,30 +2,37 @@ package main
 
 type philosopher struct{
 	eating bool
-	eatingIn chan bool
-	eatingOut chan bool
 	timesEaten int
+	leftFork *fork
+	rightFork *fork
 }
 
-func newPhilosopher() *philosopher {
-	p := philosopher{}
-	p.eating = false;
-	p.eatingIn = make(chan bool)
-	p.eatingOut = make(chan bool)
+func NewPhilosopher(leftFork *fork, rightFork *fork) *philosopher {
+	p := philosopher{leftFork: leftFork, rightFork: rightFork}
+	p.eating = false
 	p.timesEaten = 0
-
 	return &p
 }
 
-func start(p philosopher) {
+func Start(p philosopher) {
 	for {
-		if len(p.eatingIn) > 0 {
-			p.eating = <- p.eatingIn
-			if p.eating {
+		var rf, lf bool
+		select {
+		case rf <- rightFork.output:
+			rightFork.waiter.Lock()
+			select {
+			case lf <- leftFork.output:
+				leftFork.waiter.Lock()
+				p.eating = true
 				p.timesEaten++
-			} 
-		}
-		p.eatingOut <- p.eating
+				fmt.Println("Eating nam nam")
+				time.Sleep(300 * time.Millisecond)
+				leftFork.input <- 1
+				rightFork.input <- 1
+				leftFork.waiter.Unlock()
+			}
+			rightFork.waiter.Unlock()
+		}	
 	}
 }
 
