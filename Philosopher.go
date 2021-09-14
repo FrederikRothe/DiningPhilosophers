b15705ddd5562dfc693cdaf942a9e2821d5c8679
Frakty <66801011+FrederikRothe@users.noreply.github.com>
2021-09-14 08:51:28 +0200

    First implementation of philosopher.

--- Philosopher.go
+++ Philosopher.go
@@ -1,9 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
-
-func Philosopher() {
-	fmt.Println("Need code")
+type philosopher struct{
+	eating bool
+	eatingIn chan bool
+	eatingOut chan bool
+	timesEaten int
 }
+
+func newPhilosopher() *philosopher {
+	p := philosopher{}
+	p.eating = false;
+	p.eatingIn = make(chan bool)
+	p.eatingOut = make(chan bool)
+	p.timesEaten = 0
+
+	return &p
+}
+
+func start(p philosopher) {
+	for {
+		if len(p.eatingIn) > 0 {
+			p.eating = <- p.eatingIn
+			if p.eating {
+				p.timesEaten++
+			} 
+		}
+		p.eatingOut <- p.eating
+	}
+}
+
